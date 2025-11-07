9b5c5a32b1d56e0055f84f35b0c3b1915f469765
Samoyed <gus02031365@gmail.com>
2025-11-07 21:36:36 +0900

    feat(Intake) : 모집 기수 및 차수 시간적 기준을 관리를 위한 엔티티 클래스 생성

---
+++ src/main/java/army/helper/domain/Intake.java
@@ -0,0 +1,34 @@
+package army.helper.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "intakes"
+    ,uniqueConstraints = @UniqueConstraint(columnNames = {"month" , "intakeNo"}))
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Intake { //모집기수/차수 시간적 기준을 관리를 위한 엔티티
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String month;
+
+    private String intakeNo;
+
+    @Column(nullable = false)
+    private OffsetDateTime createAt;
+
+    @Column(nullable = false)
+    private OffsetDateTime updateAt;
+}
