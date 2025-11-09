fb8bd0f767776532081500dc64a56acae7350c96
Samoyed-02 <gus02031365@gmail.com>
2025-11-09 17:15:58 +0900

    feat(RequirementApiDto) : 요구사항에 응답 Dto

--- src/main/java/army/helper/dto/RequirementApiDto.java
+++ src/main/java/army/helper/dto/RequirementApiDto.java
@@ -1,14 +1,26 @@
 package army.helper.dto;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.Data;
 
 @Data
 public class RequirementApiDto {
+
+    @JacksonXmlProperty(localName = "mjstaNm") // 모집상태
     private String status;
+
+    @JacksonXmlProperty(localName = "spcCd") //  실제 XML의 특기코드 태그명
     private String specialtyCode;
+
+    @JacksonXmlProperty(localName = "spcNm") //  특기명
     private String specialtyName;
 
-    //필수 조건 필드
-    private String majorRequirement; // 학과, 전공
-    private String certificateName; // 자격증 이름
+    @JacksonXmlProperty(localName = "mojipGbnm") // 모집 구분명
+    private String mojipGbnm;
+
+    @JacksonXmlProperty(localName = "majorRqrNm") // 전공 요구
+    private String majorRequirement;
+
+    @JacksonXmlProperty(localName = "certifiRqrNm") // 자격증 요구
+    private String certificateName;
 }
