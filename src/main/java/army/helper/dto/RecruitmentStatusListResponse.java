8c62b864e173252af709e2f248adfcc0a442f2c4
Samoyed-02 <gus02031365@gmail.com>
2025-11-09 17:13:43 +0900

    feat(RecruitmentStatusListResponse) : API 데이터 형태 정의

---
+++ src/main/java/army/helper/dto/RecruitmentStatusListResponse.java
@@ -0,0 +1,38 @@
+package army.helper.dto;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlElementWrapper;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement(name = "response")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class RecruitmentStatusListResponse {
+
+    @XmlElement(name = "body")
+    private Body body;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Body {
+        @XmlElementWrapper(name = "items")
+        @XmlElement(name = "item")
+        private List<RecruitmentStatusResponse> items;
+    }
+
+    public static RecruitmentStatusListResponse build(List<RecruitmentStatusResponse> items) {
+        Body body = new Body(items);
+        return new RecruitmentStatusListResponse(body);
+    }
+}
