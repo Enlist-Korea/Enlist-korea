ee874703eb48e4a75ae54a489f87398de02ea377
Samoyed-02 <gus02031365@gmail.com>
2025-11-09 17:07:54 +0900

    feat(build.gradle.kts) : xml 형태인 API 데이터의 가공을 위한 의존성 및 security 오류 해결를 위한 의존성 부여

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.7"
+	id("org.springframework.boot") version "3.5.5"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "army"
+group = "com.militarysupport"
 version = "0.0.1-SNAPSHOT"
 description = "Demo project for Spring Boot"
 
@@ -27,26 +27,39 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
-
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.security:spring-security-test")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:postgresql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-	implementation("org.jsoup:jsoup:1.17.2")
+	implementation("mysql:mysql-connector-java:8.0.33")
+
+//	implementation("org.springframework.boot:spring-boot-starter-webflux") // WebClient 제공
+	implementation("org.springframework:spring-webflux")
+	implementation("io.projectreactor.netty:reactor-netty-http")
+
+
+	implementation("org.jsoup:jsoup:1.17.2") // 크롤링을 위한 HTML 파싱 및 데이터추출
+
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
+
+	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
+	implementation("jakarta.xml.bind:jakarta.xml.bind-api")
+	implementation("org.glassfish.jaxb:jaxb-runtime")
 }
 
 tasks.withType<Test> {
