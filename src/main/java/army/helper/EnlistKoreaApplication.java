c365fa183e04f5dbc3e60226cb6e9bec8b757e54
Samoyed-02 <gus02031365@gmail.com>
2025-11-09 17:18:37 +0900

    feat(EnlistKoreaApplication) : 실행 클래스

--- src/main/java/army/helper/EnlistKoreaApplication.java
+++ src/main/java/army/helper/EnlistKoreaApplication.java
@@ -3,16 +3,32 @@ package army.helper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+// ApplicationContextAware를 구현하여 ApplicationContext를 사용할 수 있도록 합니다.
 @EnableScheduling
 @ConfigurationPropertiesScan
-public class EnlistKoreaApplication {
+@EnableWebSecurity
+@SpringBootApplication(scanBasePackages = "army.helper")
+@EnableJpaAuditing
+public class EnlistKoreaApplication implements ApplicationContextAware {
+
+	private static ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnlistKoreaApplication.class, args);
-
 	}
 
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		EnlistKoreaApplication.applicationContext = applicationContext;
+	}
+
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
 }
