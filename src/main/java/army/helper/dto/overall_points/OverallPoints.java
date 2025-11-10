97406fa06a10f8b90c87510e62c3945ff0fe2122
Samoyed-02 <gus02031365@gmail.com>
2025-11-10 13:34:03 +0900

    feat(OverallPoints) : 각 항목별 점수를 합하여 보내줄 response dto 생성

---
+++ src/main/java/army/helper/dto/overall_points/OverallPoints.java
@@ -0,0 +1,19 @@
+package army.helper.dto.overall_points;
+
+
+public class OverallPoints {
+
+//    // ⭐️ 기술자격·면허 배점 (Max 50)
+//    private Integer qualificationScore;
+//
+//    // ⭐️ 전공학과 배점 (Max 40)
+//    private Integer majorScore;
+//
+//    // ⭐️ 출결상황 배점 (Max 5)
+//    private Integer attendanceScore;
+//
+//    // ⭐️ 가산점 최대 배점 (Max 10)
+//    private Integer bonusMaxScore;
+
+
+}
