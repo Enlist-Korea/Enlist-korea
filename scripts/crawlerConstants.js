17590d885d6713d88e40bc621192d8bf0531e9cb
OH WOONG KWON <oungtom1212@naver.com>
2025-11-11 12:08:54 +0900

    fix: 변경된 폴더 구조 추가

--- scripts/crawlerConstants.js
+++ scripts/crawlerConstants.js
@@ -1,18 +1,32 @@
 /* eslint-disable no-underscore-dangle */
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 // --- 경로 설정 ---
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // --- 기본 URL ---
-export const MMA_BASE_URL = 'https://www.mma.go.kr';
+export const MMA_BASE_URL = "https://www.mma.go.kr";
 export const CONSCRIPTION_BASE_URL = `${MMA_BASE_URL}/conscription/recruit_service/procedure/army/`;
 
 export const PATH = {
-  MMA_DATA_OUT: path.join(__dirname, '..', 'src', 'data', 'mmaData.json'),
-  MMA_NOTICE_OUT: path.join(__dirname, '..', 'src', 'data', 'mmaNotices.json'),
+  MMA_DATA_OUT: path.join(
+    __dirname,
+    "..",
+    "src",
+    "front",
+    "data",
+    "mmaData.json",
+  ),
+  MMA_NOTICE_OUT: path.join(
+    __dirname,
+    "..",
+    "src",
+    "front",
+    "data",
+    "mmaNotices.json",
+  ),
 };
 
 // --- 병무청 링크 ---
@@ -26,10 +40,10 @@ export const MMA_LINK = {
 
 // --- MMA 'mc' 코드
 export const MC_CODE = {
-  SPECIALTY: 'mma0000388',
-  NOTICE: 'usr0000127',
+  SPECIALTY: "mma0000388",
+  NOTICE: "usr0000127",
 };
 
 // --- 기타 ---
 export const USER_AGENT =
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141 Safari/537.36';
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141 Safari/537.36";
