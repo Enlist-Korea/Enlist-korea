c76b82390029e9948c3fbe63bbb14e2947ebbc16
OH WOONG KWON <oungtom1212@naver.com>
2025-11-18 00:43:08 +0900

    fix: 해병대 매핑 이름 수정
    기존 MARINE을 MARINES로 수정

--- src/front/utils/mappingForceName.js
+++ src/front/utils/mappingForceName.js
@@ -8,7 +8,7 @@ export default function getKoreanForceName(englishForceName) {
     ARMY: '육군',
     NAVY: '해군',
     AIR: '공군',
-    MARINE: '해병대',
+    MARINES: '해병대',
   };
 
   return FORCE_NAME_MAP[englishForceName] || englishForceName;
